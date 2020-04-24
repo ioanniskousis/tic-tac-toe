b07657b75575b663af5af7c12540cc2d74ca97f7
Ioannis Kousis <bonobo@otenet.gr>
2020-04-24 18:52:06 +0300

    Fixed rubocop errors

--- lib/centralizer.rb
+++ lib/centralizer.rb
@@ -1,17 +1,15 @@
 require 'io/console'
 
 class Centralizer
-
   def initialize
-    @SCREEN_SIZE = IO.console.winsize
-    @screen_width =  @SCREEN_SIZE[1]
+    @screen_size = IO.console.winsize
+    @screen_width = @screen_size[1]
   end
 
   def centralize(str)
-    pad_length = (((@screen_width - str.size) / 2) / 2 ) * 2
-    padding = String.new(" " * pad_length)
+    pad_length = (((@screen_width - str.size) / 2) / 2) * 2
+    padding = String.new(' ' * pad_length)
     padding + str
     # str
   end
-
 end
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,8 +1,8 @@
 class Interface
   def initialize
-    @VERTICAL = "\u2503"
-    @HORIZONTAL = "\u2501"
-    @CROSS = "\u254b"
+    @vertical = "\u2503"
+    @horizontal = "\u2501"
+    @cross = "\u254b"
     @cell_width = 11
     @cell_height = 5
   end
@@ -10,26 +10,20 @@ class Interface
   def draw(arr)
     pads = @cell_height / 2
     array = [] << ''
-
     array << header_line(0, arr)
     (pads - 1).times { array << padding_line }
     array << check_line(0, arr)
     pads.times { array << padding_line }
-
     array << separator
-
     array << header_line(1, arr)
     (pads - 1).times { array << padding_line }
     array << check_line(1, arr)
     pads.times { array << padding_line }
-
     array << separator
-
     array << header_line(2, arr)
     (pads - 1).times { array << padding_line }
     array << check_line(2, arr)
     pads.times { array << padding_line }
-
     array << ''
     array
   end
@@ -37,13 +31,13 @@ class Interface
   private
 
   def separator
-    dashes = String.new(@HORIZONTAL * @cell_width)
-    dashes + @CROSS + dashes + @CROSS + dashes
+    dashes = String.new(@horizontal * @cell_width)
+    dashes + @cross + dashes + @cross + dashes
   end
 
   def header_line(line_num, arr)
     spaces = String.new(' ' * @cell_width)
-    line = spaces + @VERTICAL + spaces + @VERTICAL + spaces
+    line = spaces + @vertical + spaces + @vertical + spaces
     line[0] = ((line_num * 3) + 1).to_s if arr[line_num * 3].zero?
     line[@cell_width + 1] = ((line_num * 3) + 2).to_s if arr[(line_num * 3) + 1].zero?
     line[(2 * @cell_width) + 2] = ((line_num * 3) + 3).to_s if arr[(line_num * 3) + 2].zero?
@@ -52,12 +46,12 @@ class Interface
 
   def padding_line
     spaces = String.new(' ' * @cell_width)
-    spaces + @VERTICAL + spaces + @VERTICAL + spaces
+    spaces + @vertical + spaces + @vertical + spaces
   end
 
   def check_line(line_num, arr)
     hsp = String.new(' ' * (@cell_width / 2).to_i)
-    hspv = hsp + @VERTICAL + hsp  
+    hspv = hsp + @vertical + hsp
     line = hsp + user_check(line_num * 3, arr) + hspv
     line += user_check((line_num * 3) + 1, arr) + hspv
     line += user_check((line_num * 3) + 2, arr) + hsp
@@ -67,6 +61,7 @@ class Interface
   def user_check(arr_index, arr)
     return 'X' if arr[arr_index] == 1
     return 'O' if arr[arr_index] == 2
+
     ' '
   end
 end
